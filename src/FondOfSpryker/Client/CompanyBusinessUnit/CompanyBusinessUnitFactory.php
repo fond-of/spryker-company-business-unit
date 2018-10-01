69bfcc35479244e5eaddcfc7dfe40a3846440d1f
Daniel Rose <daniel-rose@gmx.de>
2018-10-01 16:03:19 +0200

    bugfix

--- src/FondOfSpryker/Client/CompanyBusinessUnit/CompanyBusinessUnitFactory.php
+++ src/FondOfSpryker/Client/CompanyBusinessUnit/CompanyBusinessUnitFactory.php
@@ -3,13 +3,13 @@
 namespace FondOfSpryker\Client\CompanyBusinessUnit;
 
 use FondOfSpryker\Client\CompanyBusinessUnit\Zed\CompanyBusinessUnitStub;
-use FondOfSpryker\Client\CompanyBusinessUnit\Zed\CompanyBusinessUnitStubInterface;
 use Spryker\Client\CompanyBusinessUnit\CompanyBusinessUnitFactory as BaseCompanyBusinessUnitFactory;
+use Spryker\Client\CompanyBusinessUnit\Zed\CompanyBusinessUnitStubInterface;
 
 class CompanyBusinessUnitFactory extends BaseCompanyBusinessUnitFactory
 {
     /**
-     * @return \FondOfSpryker\Client\CompanyBusinessUnit\Zed\CompanyBusinessUnitStubInterface
+     * @return \Spryker\Client\CompanyBusinessUnit\Zed\CompanyBusinessUnitStubInterface
      */
     public function createZedCompanyBusinessUnitStub(): CompanyBusinessUnitStubInterface
     {
